5cf20c8d32057f3e395b9cf7b0a127bef2721c24
Sebastian Rentmeister <sence198719@gmail.com>
2026-04-02 21:57:15 +0200

    HTML der Karten Elemente verbessert

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -20,15 +20,16 @@ for animal in animals_data:
 
 output=""
 for animal in animals_data:
-    output+= '<li class="cards__item">'
-    output+= f"Name: {animal['name']}<br>\n"
+    output+= f'<li class="cards__item">'
+    output+= f'<div class="card__title">{animal['name']}</div><br>\n'
+    output+= f'<p class="card__text">'
     if "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}<br>\n"
+        output += f"<strong>Diet:</strong>{animal['characteristics']['diet']}<br>\n"
     if "locations" in animal:
-        output += f"Locations: {animal['locations'][0]}<br>\n"
+        output += f"<strong>Locations:</strong> {animal['locations'][0]}<br>\n"
     if "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}<br>\n"
-    output += "</li>"
+        output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br>\n"
+    output += "</p></li>"
 
 print(output)
 
